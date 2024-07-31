d1e83f06a9ce41f333b6832f35eca73979a5fff5
kosurumuniraja <86332515+kosurumuniraja@users.noreply.github.com>
2024-07-31 16:15:37 +0530

    Update eks-main.tf

--- eks-main.tf
+++ eks-main.tf
@@ -42,10 +42,11 @@ resource "aws_vpc" "eks_vpc" {
 
 # Create subnets
 resource "aws_subnet" "eks_subnet" {
-  count             = 2
-  vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "eks_subnet_${count.index + 1}"
